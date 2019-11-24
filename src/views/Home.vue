df98650438961c74bbb770f1a716871c93f5d627
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-11-24 20:49:09 +0100

    add images to nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,14 @@
         <mdb-col md="12">
             <mdb-navbar color="purple-gradient" class="lighten-3 py-4 mb-4">
                 <mdb-navbar-nav nav class="font-weight-bold">
-                    <mdb-nav-item href="#" active anchorClass="white-text">Active</mdb-nav-item>
+                    <mdb-nav-item href="" active anchorClass="white-text"><img src="../assets/logo-icon.png"
+                                                                               alt="login-icon">
+                    </mdb-nav-item>
                     <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
                     <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
-                    <mdb-nav-item href="#" anchorClass="white-text">Link</mdb-nav-item>
+                    <mdb-nav-item class="float-right" href="#" anchorClass="white-text">Login <img
+                            src="../assets/login-icon.png"
+                            class="img-fluid" alt="login-icon"></mdb-nav-item>
                 </mdb-navbar-nav>
             </mdb-navbar>
         </mdb-col>
@@ -28,7 +32,12 @@
     }
 </script>
 
-<style>
+<style scoped>
+
+    .white-text {
+    @include float-right !important;
+    }
+
     .nav a.white-text:hover {
         color: #e0e0e0 !important;
     }
---
+++ src/views/TestArea.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="test">
+        <div class="float-left">Float left on all viewport sizes</div>
+        <br>
+        <div class="float-right">Float right on all viewport sizes</div>
+        <br>
+        <div class="float-none">Don't float on all viewport sizes</div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "TestArea"
+    }
+</script>
+
+
+<style>
+
+</style>
