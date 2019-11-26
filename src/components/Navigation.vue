23c9890848fcaed4c6dd6b2e7062a9c09918399b
ChromeGG <imie.nazwisko69.at@gmail.com>
2019-11-26 19:07:39 +0100

    rebuild a nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,41 +1,19 @@
 <template>
     <mdb-navbar position="top" dark color="purple-gradient" scrolling>
-        <mdb-navbar-brand href="#"><img src="/src/assets/logo-icon.png" alt="logo-icon"></mdb-navbar-brand>
-        <mdb-navbar-toggler>
-            <mdb-navbar-nav>
-                <mdb-nav-item href="#" waves-fixed>Home</mdb-nav-item>
-                <mdb-nav-item href="#" waves-fixed>Features</mdb-nav-item>
-                <mdb-nav-item href="#" waves-fixed>Pricing</mdb-nav-item>
-                <!-- Dropdown -->
-                <mdb-dropdown tag="li" class="nav-item">
-                    <mdb-dropdown-toggle tag="a" navLink slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
-                    <mdb-dropdown-menu>
-                        <mdb-dropdown-item>Action</mdb-dropdown-item>
-                        <mdb-dropdown-item>Another action</mdb-dropdown-item>
-                        <mdb-dropdown-item>Something else here</mdb-dropdown-item>
-                    </mdb-dropdown-menu>
-                </mdb-dropdown>
-            </mdb-navbar-nav>
-            <!-- Search form -->
-
+        <mdb-navbar-brand href="#"><img src="../assets/logo-icon.png" alt="logo-icon"></mdb-navbar-brand>
+        <mdb-navbar-toggler class="d-flex justify-content-end">
+            <div>
+                <mdb-navbar-nav>
+                    <mdb-nav-item href="#" waves-fixed>Login <img src="../assets/login-icon.png" alt="login-icon">
+                    </mdb-nav-item>
+                </mdb-navbar-nav>
+            </div>
         </mdb-navbar-toggler>
     </mdb-navbar>
 </template>
 
 <script>
-    import {
-        mdbBtn,
-        mdbDropdown,
-        mdbDropdownItem,
-        mdbDropdownMenu,
-        mdbDropdownToggle,
-        mdbInput,
-        mdbNavbar,
-        mdbNavbarBrand,
-        mdbNavbarNav,
-        mdbNavbarToggler,
-        mdbNavItem
-    } from 'mdbvue';
+    import {mdbNavbar, mdbNavbarBrand, mdbNavbarNav, mdbNavbarToggler, mdbNavItem} from 'mdbvue';
 
     export default {
         name: 'NavigationPage',
@@ -44,29 +22,16 @@
             mdbNavItem,
             mdbNavbarNav,
             mdbNavbarToggler,
-            mdbDropdown,
-            mdbDropdownItem,
-            mdbDropdownMenu,
-            mdbDropdownToggle,
-            // eslint-disable-next-line vue/no-unused-components
-            mdbInput,
-            // eslint-disable-next-line vue/no-unused-components
-            mdbBtn,
             mdbNavbarBrand
         }
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .view {
-        background: url("https://mdbootstrap.com/img/Photos/Others/img (42).jpg") no-repeat center center;
-        background-size: cover;
-        height: 100%;
-    }
 
     .navbar .dropdown-menu a:hover {
         color: inherit !important;
     }
+
 </style>
 
